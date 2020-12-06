b53380ec0da182393861477f9fc3cf84deba41d0
Necronomicon2401 <45945914+Necronomicon2401@users.noreply.github.com>
2020-12-06 19:55:03 +0300

    webdriver

---
+++ Webdriver/src/test/java/SuperlamaTest.java
@@ -0,0 +1,40 @@
+import org.apache.tools.ant.types.Assertions;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+
+public class SuperlamaTest extends Assertions {
+    public static final String FIRST_LINK = "https://superlama.by/kulon-harry-potter";
+
+    @Test
+    public void testAddedLikedItems() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(FIRST_LINK);
+
+        WebElement inputQuantity = driver.findElement(By.xpath("//input[@class='quantity-productcart']"));
+        inputQuantity.sendKeys("0");
+
+        WebElement addingInBasketButton = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='В корзину']")));
+        addingInBasketButton.click();
+
+        WebElement goToOrderPageButton = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@style='float: right']")));
+        goToOrderPageButton.click();
+
+        WebElement itemsInOrder = driver.findElement(By.xpath("//input[@name='quantity[2433]']"));
+
+        Assert.assertEquals("10", itemsInOrder.getAttribute("value"));
+
+    }
+
+
+
+}
